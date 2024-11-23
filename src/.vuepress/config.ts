c9aaec02a6bd56ba8b9a19b4823c14e28e841598
锦衣 <hengxing1774006@163.com>
2024-11-23 12:19:03 +0800

    up

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { umamiAnalyticsPlugin } from "@vuepress/plugin-umami-analytics";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -9,6 +10,13 @@ export default defineUserConfig({
   description: "锦衣小窝",
   theme,
   plugins: [
+    // Umami Analytics 插件
+    umamiAnalyticsPlugin({
+      trackerUrl: "https://umami-t3sl.vercel.app/script.js", // Umami 跟踪脚本地址
+      websiteId: "b87b08c8-683b-4775-a8f3-4c72c0f4879f",      // Umami 网站 ID
+    }),
+
+    // 搜索插件
     searchProPlugin({
       indexContent: true,
       customFields: [
@@ -32,36 +40,39 @@ export default defineUserConfig({
   head: [
     // 添加 jQuery 脚本
     [
-      'script',
+      "script",
       {
-        src: 'https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js',
+        src: "https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js",
         async: true,
       },
     ],
     // 添加 Font Awesome 样式
     [
-      'link',
+      "link",
       {
-        rel: 'stylesheet',
-        href: 'https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css',
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css",
       },
     ],
     // 添加 Live2D 小部件脚本
     [
-      'script',
+      "script",
       {
-        src: 'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js',
+        src: "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js",
         async: true,
       },
     ],
     // 引入 base.scss 文件
     [
-      'link',
+      "link",
       {
-        rel: 'stylesheet/scss',
-        href: '/.vuepress/styles/base.css', // 确保这个路径正确
+        rel: "stylesheet/scss",
+        href: "/.vuepress/styles/base.css", // 确保这个路径正确
       },
     ],
   ],
 });
+
+
+
 
